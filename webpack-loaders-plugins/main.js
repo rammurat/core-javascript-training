b72ba967be9372a1b17abd15e538d353b29ca892
Ram Murat Sharma <rammurat.rms.sharma0@gmail.com>
2020-04-01 17:30:22 +0100

    Webpack loaders and plugins added

---
+++ webpack-loaders-plugins/main.js
@@ -0,0 +1,4 @@
+import {sum} from './module';
+
+const calculateSum = sum(5,5);
+
---
+++ webpack-loaders-plugins/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const path = require('path');
+
+const handler = (percentage, message, ...args) => {
+  // e.g. Output each progress message directly to the console:
+  console.info(percentage, message, ...args);
+};
+
+module.exports = {
+  entry: './main.js', // main entry file, where all other modules must be loading
+  output: {
+    path: path.resolve(__dirname, 'dist'), // new files created on the fly (old files removed) 
+    filename: 'bundle.js'
+  },
+  mode: 'development', // production is by default
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new webpack.ProgressPlugin(handler)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  }
+};
